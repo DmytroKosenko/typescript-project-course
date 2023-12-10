bf70289bd47cbb999a292621dfe5d0d50e8fbe84
Dmytro <109982700+DmytroKosenko@users.noreply.github.com>
2023-12-10 16:37:15 +0200

    first_part_of_learning_section

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log("Your code goes here...");
+var product = {
+    id: "abc1",
+    price: 12.99,
+    tags: ["great-offer", "hot-and-new"],
+    details: {
+        title: "Red Carpet",
+        description: "A great carpet - almost brand-new!",
+    },
+};
+var person = {
+    name: "Max",
+    age: 30,
+    hobbies: ["Sports", "Cooking"],
+};
+var favpuriteActivities; //any for all types, or we can make it for specific type, like 'string'
+favpuriteActivities = ["Sports", 1];
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
--- app.ts
+++ app.ts
@@ -1,1 +1,30 @@
-console.log("Your code goes here...");
+const product = {
+  id: "abc1",
+  price: 12.99,
+  tags: ["great-offer", "hot-and-new"],
+  details: {
+    title: "Red Carpet",
+    description: "A great carpet - almost brand-new!",
+  },
+};
+
+const person = {
+  name: "Max",
+  age: 30,
+  hobbies: ["Sports", "Cooking"],
+  role: [2, "author"],
+};
+
+person.role.push("admin");
+person.role[1] = 10;
+
+let favouriteActivities: any[]; //any for all types, or we can make it for specific type, like 'string'
+
+favouriteActivities = ["Sports", 1];
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+  //console.log(hobby.map()); // return an error!
+}
